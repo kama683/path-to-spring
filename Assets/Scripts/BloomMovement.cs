cea13e372baa10ff153f865b1e91fa6098d13cc5
Bauyrzhan Issayev <isaev.bekbolat15@gmail.com>
2026-03-12 07:02:21 +0500

    add two player sprites and game mechanizms

--- Assets/Scripts/BloomMovement.cs
+++ Assets/Scripts/BloomMovement.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class BloomMovement : MonoBehaviour
 {
+    private Animator animator;
+
+    [Header("References")]
+    public Transform groundCheck;
+    public Transform visual;
+
     [Header("Movement")]
     public float moveSpeed = 5f;
     public float airControlMultiplier = 0.9f;
 
     [Header("Jump")]
     public float jumpForce = 11f;
-    public float groundCheckRadius = 0.25f;
+    public float groundCheckRadius = 0.12f;
     public LayerMask groundLayer;
-    public Transform groundCheck;
 
     [Header("Jump Feel")]
     public float coyoteTime = 0.12f;
@@ -25,9 +30,15 @@ public class BloomMovement : MonoBehaviour
     private float coyoteTimeCounter;
     private float jumpBufferCounter;
 
+    private Vector3 visualScale;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+
+        if (visual != null)
+            visualScale = visual.localScale;
     }
 
     void Update()
@@ -37,6 +48,8 @@ public class BloomMovement : MonoBehaviour
         UpdateTimers();
         HandleJump();
         BetterJumpPhysics();
+        UpdateAnimator();
+        HandleFlip();
     }
 
     void FixedUpdate()
@@ -104,6 +117,29 @@ public class BloomMovement : MonoBehaviour
         else if (rb.linearVelocity.y > 0f && !Input.GetKey(KeyCode.W))
         {
             rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpGravityMultiplier - 1f) * Time.deltaTime;
+        }
+    }
+
+    void UpdateAnimator()
+    {
+        if (animator == null) return;
+
+        animator.SetFloat("Speed", Mathf.Abs(moveInput));
+        animator.SetBool("IsGrounded", isGrounded);
+        animator.SetFloat("VerticalVelocity", rb.linearVelocity.y);
+    }
+
+    void HandleFlip()
+    {
+        if (visual == null) return;
+
+        if (moveInput > 0.01f)
+        {
+            visual.localScale = new Vector3(Mathf.Abs(visualScale.x), visualScale.y, visualScale.z);
+        }
+        else if (moveInput < -0.01f)
+        {
+            visual.localScale = new Vector3(-Mathf.Abs(visualScale.x), visualScale.y, visualScale.z);
         }
     }
 
